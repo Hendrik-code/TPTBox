52f2bc04057f497f5b24a5f359cc35c6378c656c
robert <robert.graf@tum.de>
2024-02-12 13:44:16 +0100

    remove old registation version

--- TPTBox/registration/ridged_intensity/__register_ants.py
+++
@@ -1,4 +0,0 @@
-from typing import Literal
-
-Similarity_Measures = Literal["slr", "mi", "pmi", "dpmi", "cc", "cr", "crl1"]
-Affine_Transforms = Literal["affine", "affine2d", "similarity", "similarity2d", "rigid", "rigid2d"]
