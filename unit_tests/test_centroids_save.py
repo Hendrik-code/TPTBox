7163fa8ac565b2041ec464f6c97794d61cfc8d72
robert <robert.graf@tum.de>
2024-02-21 10:29:46 +0100

    updated test

--- unit_tests/test_centroids_save.py
+++ unit_tests/test_centroids_save.py
@@ -2,23 +2,20 @@
 # coverage run -m unittest
 # coverage report
 # coverage html
-import sys
-import tempfile
-from pathlib import Path
-
-file = Path(__file__).resolve()
-sys.path.append(str(file.parents[2]))
 import os
 import random
+import sys
+import tempfile
 import unittest
+from pathlib import Path
 
 import nibabel as nib
 import numpy as np
 
 import TPTBox.core.bids_files as bids
-from TPTBox import Centroids
-from TPTBox.core.nii_wrapper import NII
-from TPTBox.core.poi import *
+from TPTBox import POI
+from TPTBox.core.nii_wrapper import NII, Ax_Codes
+from TPTBox.core.vert_constants import conversion_poi2text
 
 repeats = 20
 
@@ -34,22 +31,22 @@ def get_random_shape():
     return tuple(int(1000 * random.random() + 10) for _ in range(3))  # type: ignore
 
 
-def get_centroids(x: tuple[int, int, int] = (50, 30, 40), num_point=3):
+def get_centroids(x: tuple[int, int, int] = (50, 30, 40), num_point=3, default_sub=None):
     out_points: dict[tuple[int, int], tuple[float, float, float]] = {}
 
-    for idx in range(num_point):
+    for _ in range(num_point):
         point = tuple(random.randint(1, a * 100) / 100.0 for a in x)
-        out_points[random.randint(1, 256), random.randint(1, 256)] = point
-    return Centroids(out_points, get_random_ax_code(), zoom=(random.random() * 3, random.random() * 3, random.random() * 3), shape=x)
+        out_points[random.randint(1, 256), random.randint(1, 256) if default_sub is None else 50] = point
+    return POI(out_points, get_random_ax_code(), zoom=(random.random() * 3, random.random() * 3, random.random() * 3), shape=x)
 
 
 def get_centroids2(x: tuple[int, int, int] = (50, 30, 40), num_point=3):
     out_points: dict[tuple[int, int], tuple[float, float, float]] = {}
     l = list(conversion_poi2text.keys())
-    for idx in range(num_point):
+    for _ in range(num_point):
         point = tuple(random.randint(1, a * 100) / 100.0 for a in x)
         out_points[random.randint(1, 27), l[random.randint(0, len(l) - 1)]] = point
-    return Centroids(out_points, get_random_ax_code(), zoom=(1, 1, 1), shape=x)
+    return POI(out_points, get_random_ax_code(), zoom=(1, 1, 1), shape=x)
 
 
 s = Path("BIDS/test/")
@@ -63,7 +60,7 @@ class Test_Centroids(unittest.TestCase):
             p = Path(s, "test_save_0.json")
             cdt = get_centroids(x=(500, 700, 900), num_point=99)
             cdt.save(p, verbose=False)
-            cdt2 = Centroids.load(p)
+            cdt2 = POI.load(p)
             self.assertEqual(cdt, cdt2)
             p.unlink()
 
@@ -72,7 +69,7 @@ class Test_Centroids(unittest.TestCase):
             p = Path(s, "test_save_1.json")
             cdt = get_centroids2(x=get_random_shape(), num_point=99)
             cdt.save(p, verbose=False, save_hint=1)
-            cdt2 = Centroids.load(p)
+            cdt2 = POI.load(p)
             self.assertEqual(cdt, cdt2)
             p.unlink()
 
@@ -81,7 +78,7 @@ class Test_Centroids(unittest.TestCase):
             p = Path(s, "test_save_2.json")
             cdt = get_centroids(x=get_random_shape(), num_point=99)
             cdt.save(p, verbose=False, save_hint=2)
-            cdt2 = Centroids.load(p)
+            cdt2 = POI.load(p)
             self.assertEqual(cdt, cdt2)
             Path(p).unlink()
 
@@ -90,7 +87,7 @@ class Test_Centroids(unittest.TestCase):
             p = Path(s, "test_save_10.json")
             cdt = get_centroids(x=get_random_shape(), num_point=99)
             cdt.save(p, verbose=False, save_hint=10)
-            cdt2 = Centroids.load(p)
+            cdt2 = POI.load(p)
             cdt = cdt.rescale((1, 1, 1), verbose=False).reorient_(("R", "P", "I"))
             self.assertEqual(cdt, cdt2)
             Path(p).unlink()
@@ -100,9 +97,9 @@ class Test_Centroids(unittest.TestCase):
             p = Path(s, "test_save_all.json")
             cdt = get_centroids2(x=get_random_shape(), num_point=99)
             cdt2 = cdt
-            for i in range(5):
+            for _ in range(5):
                 cdt2.save(p, verbose=False, save_hint=random.randint(0, 2))
-                cdt2 = Centroids.load(p)
+                cdt2 = POI.load(p)
             self.assertEqual(cdt, cdt2)
             Path(p).unlink()
 
