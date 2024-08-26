ea70913e8ab25ce634c192cc9709f29746cbb3e5
iback <hendrik.moeller@tum.de>
2024-08-26 12:36:50 +0000

    remove debug prints. added offset functionality, size_factor can be float

--- TPTBox/mesh3D/mesh.py
+++ TPTBox/mesh3D/mesh.py
@@ -49,7 +49,6 @@ class Mesh3D:
         return Mesh3D(mesh)
 
     def show(self):
-        print("1")
         pv.start_xvfb()
         pl = pv.Plotter()
         pl.set_background("black", top=None)
@@ -58,7 +57,6 @@ class Mesh3D:
         pv.global_theme.interactive = True
 
         pl.add_mesh(self.mesh)
-        print(2)
         pl.show()
 
 
@@ -75,23 +73,35 @@ class SegmentationMesh(Mesh3D):
         if np.issubdtype(int_arr.dtype, np.floating):
             print("input is of type float, converting to int")
             int_arr.astype(np.uint16)
-        print("calculate bounding box cutout")
         # calculate bounding box cutout
         bbox_crop = np_bbox_binary(int_arr, px_dist=2)
         x1, y1, z1 = bbox_crop[0].start, bbox_crop[1].start, bbox_crop[2].start
         arr_cropped = int_arr[bbox_crop]
 
         vertices, faces, normals, values = marching_cubes(arr_cropped, gradient_direction="ascent", step_size=1)
+        self._faces = faces
+        self._normals = normals
+        self._values = values
+        self._x1 = x1
+        self._y1 = y1
+        self._z1 = z1
+        #
         vertices += (x1, y1, z1)  # so it has correct relative coordinates (not world coordinates!)
-        print("column_stack")
-
+        self._vertices = vertices
         vfaces = np.column_stack((np.ones(len(faces)) * 3, faces)).astype(int)
-
-        mesh = pv.PolyData(vertices, vfaces)
+        mesh = pv.PolyData(self._vertices, vfaces)
         mesh["Normals"] = normals
         mesh["values"] = values
-        print("mesh")
         self.mesh = mesh
+
+    def get_mesh_with_offset(self, offset: tuple[float, float, float]):
+        vertices = self._vertices + offset
+        vfaces = np.column_stack((np.ones(len(self._faces)) * 3, self._faces)).astype(int)
+
+        mesh = pv.PolyData(vertices, vfaces)
+        mesh["Normals"] = self._normals
+        mesh["values"] = self._values
+        return mesh
 
     @classmethod
     def from_segmentation_nii(cls, seg_nii: NII, rescale_to_iso: bool = True):
@@ -110,7 +120,7 @@ class POIMesh(Mesh3D):
         rescale_to_iso: bool = True,
         regions: list[int] | None = None,
         subregions: list[int] | None = None,
-        size_factor: int = 5,
+        size_factor: float = 5,
     ) -> None:
         poi.reorient_()
         if rescale_to_iso:
@@ -122,17 +132,27 @@ class POIMesh(Mesh3D):
         if subregions is None:
             subregions = poi.keys_subregion()
 
-        poi_extracted: list[COORDINATE] = []
+        self.poi_extracted: list[COORDINATE] = []
+        self.size_factor = size_factor
 
         for r_id, s_id, coord in poi.items():
             if r_id in regions and s_id in subregions:
-                poi_extracted.append(coord)
+                self.poi_extracted.append(coord)
 
-        n = pv.PolyData(poi_extracted)
-        n["radius"] = np.ones(shape=len(poi_extracted)) * size_factor
+        assert len(self.poi_extracted) > 0, "no POIs present"
+        n = pv.PolyData(self.poi_extracted)
+        n["radius"] = np.ones(shape=len(self.poi_extracted)) * size_factor
         geom = pv.Sphere(theta_resolution=8, phi_resolution=8)
         glyphed = n.glyph(scale="radius", geom=geom, progress_bar=False, orient=False)
         self.mesh = glyphed
+
+    def get_mesh_with_offset(self, offset: tuple[float, float, float]):
+        pois_shifted = [(x + offset[0], y + offset[1], z + offset[2]) for x, y, z in self.poi_extracted]
+        n = pv.PolyData(pois_shifted)
+        n["radius"] = np.ones(shape=len(pois_shifted)) * self.size_factor
+        geom = pv.Sphere(theta_resolution=8, phi_resolution=8)
+        glyphed = n.glyph(scale="radius", geom=geom, progress_bar=False, orient=False)
+        return glyphed
 
 
 if __name__ == "__main__":
