147c44891c06ae982741a5728c38d4165c205535
ga84mun <robert.graf@tum.de>
2025-04-11 09:36:14 +0000

    git add add new registration Method.

---
+++ TPTBox/registration/deepali/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+try:
+    from .spine_rigid_elements_reg import Rigid_Elements_Registration
+
+except Exception:
+    pass
+
+try:
+    from .deepali_model import General_Registration
+
+except Exception:
+    pass
