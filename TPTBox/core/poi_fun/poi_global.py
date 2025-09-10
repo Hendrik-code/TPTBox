bda5faad8b585fa4b1adcc12f7d4efb019784447
gu53lif <tanja.lerchl@tum.de>
2025-09-10 14:13:43 +0200

    ruff

--- TPTBox/core/poi_fun/poi_global.py
+++ TPTBox/core/poi_fun/poi_global.py
@@ -68,12 +68,12 @@ class POI_Global(Abstract_POI):
     @property
     def origin(self):
         return (0, 0, 0)
-    
+
     @property
     def orientation(self):
         if self.itk_coords:
-            return ("L","A","S")
-        return ("R","P","S")
+            return ("L", "A", "S")
+        return ("R", "P", "S")
 
     @property
     def is_global(self) -> bool:
