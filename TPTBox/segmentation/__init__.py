f05f9cc96050231f38e6e4b31132f5c5871ec22f
ga84mun <robert.graf@tum.de>
2025-10-17 09:32:42 -0400

    remove total from name

--- TPTBox/segmentation/__init__.py
+++ TPTBox/segmentation/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
 
 from TPTBox.segmentation.spineps import run_spineps_all, run_spineps_single
-from TPTBox.segmentation.TotalVibeSeg import run_totalvibeseg
+from TPTBox.segmentation.VibeSeg import run_vibeseg
+from TPTBox.segmentation.VibeSeg import run_vibeseg as run_totalvibeseg  # TODO deprecate
